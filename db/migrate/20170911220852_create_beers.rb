b1850383e00558ecdfd27ed55e5c5245b56331f7
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-11 18:15:48 -0400

    add migration file

--- db/migrate/20170911220852_create_beers.rb
+++ db/migrate/20170911220852_create_beers.rb
@@ -1,7 +1,10 @@
 class CreateBeers < ActiveRecord::Migration[5.1]
   def change
     create_table :beers do |t|
-
+      t.string :name
+      t.string :desc
+      t.string :abv
+      
       t.timestamps
     end
   end
