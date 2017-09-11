df15931165be0f211df920d23cdfb1ad05d5ecd8
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-11 18:40:28 -0400

    BIG FAT COMMIT

--- db/migrate/20170911220852_create_beers.rb
+++ db/migrate/20170911220852_create_beers.rb
@@ -4,8 +4,10 @@ class CreateBeers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :desc
       t.string :abv
-      
+      t.references :company, foreign_key: true
+
       t.timestamps
     end
+    add_index :beers, :company_id
   end
 end
