85b346b7f61f98bbc5f170159ccca84f596894b4
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-11 18:13:31 -0400

    generate views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'beers/index'
+
+  get 'beers/show'
+
+  get 'beers/new'
+
+  get 'beers/create'
+
+  get 'beers/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
