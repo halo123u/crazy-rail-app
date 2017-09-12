29919d35882f1ab3339846c194628e9736a79f62
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-11 20:15:05 -0400

    add controller for companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,16 @@
+class CompaniesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+
+  get 'companies/edit'
+
+  get 'companies/create'
+
+  get 'companies/new'
+
+  get 'companies/show'
+
   resources :companies
   resources :beers 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
